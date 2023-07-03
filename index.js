a7ef93408098f12d53ca38e9a19ff207d0a91239
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-07-03 15:22:48 +0530

    deploy

---
+++ index.js
@@ -0,0 +1,15 @@
+const json_server= require("json-server")
+const path=require("path");
+const cors= require("cors");
+
+const server= json_server.create();
+const router= json_server.router(path.join(__dirname,"db.json"))
+
+server.use(cors())
+server.use(json_server.bodyParser)
+server.use(json_server.defaults())
+server.use(router)
+
+server.listen(3000, ()=>{
+    console.log("JSON Server is running")
+})
