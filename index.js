0f1677646385bdba92003d0958ab7283fef5f30d
shubham-Adhya <119453418+shubham-Adhya@users.noreply.github.com>
2023-07-03 15:35:07 +0530

    cors updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const json_server= require("json-server")
 const path=require("path");
 const cors= require("cors");
-server.use(cors())
 
 const server= json_server.create();
+server.use(cors())
 const router= json_server.router(path.join(__dirname,"db.json"))
 
 server.use(json_server.bodyParser)
